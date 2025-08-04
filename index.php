603d27526e38709ed65bf40546c541fafee423c0
BGRteam <82728754+mrpov-sys@users.noreply.github.com>
2025-08-04 10:21:02 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html><html><head>
+<title>hacked by agenmassive</title>
+<link href="https://fonts.googleapis.com/css?family=Inconsolata&amp;display=swap" rel="stylesheet">
+<meta charset="UTF-8">
+<meta name="description" content="Karawang Cyber Team">
+<meta name="keywords" content="Karawang Cyber Team">
+<meta http-equiv="cache-control" content="index,cache">
+<meta http-equiv="pragma" content="index,cache">
+<meta name="theme-color" content="black">
+<style></style><style type="text/css">* {user-select: auto !important; -webkit-user-select: auto !important;}</style><input type="hidden" id="inject_idm_text_selection"></head>
+<body bgcolor="black"><center>
+
+<div style="height: auto; min-height: 100%;">
+<div style="text-align: center; width:800px; margin-left: -400px; position: absolute; top: 20%; left: 50%;">
+<i><b>
+<font size="4" face="Inconsolata" color="white">root@massive</a>:~$ ./wh00pz?!🇮🇩 <br>
+<img src="https://asset.kompas.com/crops/2RnkgZTEZDB1wrkTTVewExs-e24=/22x0:610x392/1200x800/data/photo/2023/10/25/6538cf6057235.png" width="550" height="250">
+<br>
+Touch By AgenMassive.1up <br>
+<font face="Inconsolata" size="4" color="white"><br>
+" Katanya Merah Putih lambang persatuan, tapi yang angkat bendera perlawanan, dicap pemecah belah bangsa oleh yang takut perubahan. "
+<br><br>
+<font face="Inconsolata" size="4" color="red">
+Bendera One Piece bukan ajakan perang, tapi simbol rakyat yang muak dipermainkan sistem. Pengibaran serempak bukan makar, itu suara-yang selama ini dipaksa diam. </font></html>
+<br><br>
+	<font face="Inconsolata" size="4" color="white">
+Tolong bedakan antara melawan ketidakadilan dan menghancurkan persatuan. </font></html>
+<script language="JavaScript">
+<!--
+
+//Disable right mouse click Script
+//By Maximus (maximus@nsimail.com) w/ mods by DynamicDrive
+//For full source code, visit http://www.dynamicdrive.com
+
+var message="wkwkwk?";
+
+///////////////////////////////////
+function clickIE4(){
+if (event.button==2){
+alert(message);
+return false;
+}
+}
+
+function clickNS4(e){
+if (document.layers||document.getElementById&&!document.all){
+if (e.which==2||e.which==3){
+alert(message);
+return false;
+}
+}
+}
+
+if (document.layers){
+document.captureEvents(Event.MOUSEDOWN);
+document.onmousedown=clickNS4;
+}
+else if (document.all&&!document.getElementById){
+document.onmousedown=clickIE4;
+}
+
+
+document.oncontextmenu=new Function("alert(message);return false")
+
+// -->
+</script>
+<script>(function(c,a,d){var b=function(f,e){this.el=a.querySelectorAll(f)[0];this.config=e||{};this.lyric=e.lyric||"";this.newLyric="";this.showUpSpeed=e.showUpSpeed||0;this.flashSpeed=e.flashSpeed||0;this.flashTimeout=e.flashTimeout||0;this.showOut()};b.prototype={showOut:function(){var f=this,g=0,e,h="";if(f.flashSpeed){f.newLyric=e=f.replaceFun(f.lyric,f.lyric.split(""))}else{f.newLyric=e=f.lyric}if(f.showUpSpeed){var j=setInterval(function(){h+=e.split("")[g];g++;f.newLyric=h;f.el.innerHTML=h;f.el.dataset.text=h;if(f.lyric.split("").length===g){clearInterval(j);setTimeout(function(){f.flash(f.newLyric)},f.flashTimeout)}},f.showUpSpeed)}else{f.el.innerHTML=e;f.el.dataset.text=e;setTimeout(function(){f.flash(f.newLyric)},f.flashTimeout)}},replaceFun:function(f,e){return f.split("").map(function(h){var g=e.filter(function(j,i){return e.indexOf(j)==i});return h=g[Math.floor((Math.random()*g.length))]}).join("")},flash:function(){var f=this,e=f.lyric.split(""),h=f.newLyric.split(""),g=f.flashSpeed;var i=setInterval(function(){if(h.join("")!==e.join("")){for(var j in e){if(h[j]!==e[j]){var k=e.filter(function(m,l){return e.indexOf(m)==l});h[j]=k[Math.floor((Math.random()*k.length))];f.el.innerHTML=f.el.dataset.text=h.join("")}}}else{clearInterval(i);f.callback()}},g)},callback:function(){if(this.config.callback){this.config.callback()}}};if(typeof module==="object"&&typeof module.exports==="object"){module.exports=exports=b}else{if(typeof define==="function"&&define.amd){define(function(){return b})}else{if(typeof c==="object"){c.Motto=b}}}})(window,document);
+var motto = new Motto('.title', {
+lyric: "magical of system",
+showUpSpeed: 85,
+flashSpeed: 70,
+flashTimeout: 1000,
+callback: function () {
+}
+});
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<p>
+
+
+
+
+
+
+
+
+
+
+<audio id="sec" __idm_id__="823202817"> <source src="https://raw.githubusercontent.com/Fingkii/Fingkii/refs/heads/main/MANUSIA%20BIASA%20(DRILL%20REMIX)%20%5BPROD.%200LANDRYS%5D.mp3"></audio>
+
+
+
+
+
+
+
+<script>
+
+
+
+var x = document.getElementById("sec");
+
+
+
+function play() {
+
+
+
+x.play();
+
+
+
+}
+
+
+
+function myFunction() {
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+function changeImage() {
+
+
+
+if (document.getElementById("imgClickAndChange").src == "https://cutewallpaper.org/21/anime-suicidal/Russian-State-Media-Says-Anime-Promotes-Teen-Suicide.jpg") {
+
+
+
+document.getElementById("imgClickAndChange").src = "https://cutewallpaper.org/21/anime-suicidal/Russian-State-Media-Says-Anime-Promotes-Teen-Suicide.jpg";
+
+
+
+}
+
+
+
+else { document.getElementById("imgClickAndChange").src = "https://cutewallpaper.org/21/anime-suicidal/Russian-State-Media-Says-Anime-Promotes-Teen-Suicide.jpg";
+
+
+
+}
+
+
+
+}
+
+
+
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</p></div></pre></font></td></tr></tbody></table><iframe scrolling="no" frameborder="0" allowtransparency="true" src="https://platform.twitter.com/widgets/widget_iframe.2f70fb173b9000da126c79afe2098f02.html?origin=http%3A%2F%2Fzonehmirrors.org" title="Twitter settings iframe" style="display: none;"></iframe><iframe id="rufous-sandbox" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" style="position: absolute; visibility: hidden; display: none; width: 0px; height: 0px; padding: 0px; border: none;" title="Twitter analytics iframe"></iframe></body></html>
